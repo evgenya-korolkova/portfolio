4b3607f7fc6afc88ee9a37e500bd4dc0405718fc
Evgenya <evgenyakorolkova@mail.ru>
2020-06-17 23:35:41 +0400

    mobile for Contacts

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
-
-  /*
+    /*
   1) получить кноку Меню, по которой будет клик
   2) получить мобильное меню
   3) прослушать клик по кнопке Меню и по клику то добавлять, то удалять класс active у моб меню
@@ -12,49 +11,109 @@ $(document).ready(function () {
   2) у всех дивов убрать класс active
     */
 
-    const mobMenuToggle = document.querySelector('.toggle-menu');
-    const mobMenu = document.querySelector('.nav_list');
-    const overlay = document.querySelector('#overlay');
-    const body = document.querySelector('body');
+    const mobMenuToggle = document.querySelector(".toggle-menu");
+    const mobMenu = document.querySelector(".nav_list");
+    const overlay = document.querySelector("#overlay");
+    const body = document.querySelector("body");
 
-    mobMenuToggle.addEventListener('click', function(){
-      mobMenu.classList.toggle('active');
-      this.classList.toggle('active');
-      overlay.classList.toggle('active');
-      body.classList.toggle('none-scroll');
-    })
-
-    window.addEventListener('resize', function(){
-      mobMenu.classList.remove('active')
-      mobMenuToggle.classList.remove('active')
-      overlay.classList.remove('active')
-      body.classList.remove('none-scroll');
-    })
-
-
-  // --- \/ fake-placeholder ---
-  const formInputs = document.querySelectorAll(".form-field");
-
-  for (let item of formInputs) {
-    const inputPlaceholder = item.nextElementSibling;
-
-    item.addEventListener("click", function () {
-      inputPlaceholder.classList.add("active");
+    mobMenuToggle.addEventListener("click", function () {
+        mobMenu.classList.toggle("active");
+        this.classList.toggle("active");
+        overlay.classList.toggle("active");
+        body.classList.toggle("none-scroll");
     });
 
-    item.addEventListener("focus", function () {
-      inputPlaceholder.classList.add("active");
+    window.addEventListener("resize", function () {
+        mobMenu.classList.remove("active");
+        mobMenuToggle.classList.remove("active");
+        overlay.classList.remove("active");
+        body.classList.remove("none-scroll");
     });
 
-    item.addEventListener("blur", function () {
-      if (this.value == "") {
-        inputPlaceholder.classList.remove("active");
-      }
+    // --- \/ fake-placeholder ---
+    const formInputs = document.querySelectorAll(".form-field");
+
+    for (let item of formInputs) {
+        const inputPlaceholder = item.nextElementSibling;
+
+        item.addEventListener("click", function () {
+            inputPlaceholder.classList.add("active");
+        });
+
+        item.addEventListener("focus", function () {
+            inputPlaceholder.classList.add("active");
+        });
+
+        item.addEventListener("blur", function () {
+            if (this.value == "") {
+                inputPlaceholder.classList.remove("active");
+            }
+        });
+    }
+    // --- /\ fake-placeholder ---
+
+    // --- Form validate ---
+    $("#contact-form").validate({
+        rules: {
+            email: {
+                required: true,
+                email: true,
+            },
+            theme: {
+                required: true,
+            },
+            message: {
+                required: true,
+            },
+        },
+
+        messages: {
+            email: {
+                required: "Введите Ваш email",
+                email: "Некорректный email",
+            },
+            theme: {
+                required: "Введите тему сообщения",
+            },
+            message: {
+                required: "Введите текст сообщения",
+            },
+        },
+
+        submitHandler: function (form) {
+            ajaxFormSubmit();
+        }
+
     });
-  }
-  // --- /\ fake-placeholder ---
+
+    // Функция AJAX запроса на сервер
+
+    function ajaxFormSubmit() {
+
+        let string = $("#contact-form").serialize(); // Соханяем данные введенные в форму в строку.
+
+        //Формируем ajax запрос
+        $.ajax({
+            type: "POST", // Тип запроса - POST
+            url: "php/mail.php", // Куда отправляем запрос
+            data: string, // Какие даные отправляем, в данном случае отправляем переменную string
+
+            // Функция если все прошло успешно
+            success: function (html) {
+                $("#contact-form").slideUp(800);
+                $('#answer').html(html);
+            }
+        });
+
+        // Чтобы по Submit больше ничего не выполнялось - делаем возврат false чтобы прервать цепчку срабатывания остальных функций
+        return false;
+    }
+    
 
 });
+
+
+
 
 // $(document).ready(function () {
 
